31eb7507dd1b45f2112a081de6d1f6e30ddefe58
FMark207 <mark.farkas.207@gmail.com>
2025-10-21 12:52:57 +0200

    kesz van hell yeah

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import BeerCard from "./components/BeerCard"
+import BeerCard, { type BeerType } from "./components/BeerCard"
+import { useEffect, useState } from "react"
+
 
 const App = () => {
+
+    const [beers, setBeers] = useState<BeerType[]>([])
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(res => res.json())
+    .then(data => setBeers(data))
+  }, [])
+
+
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {beers.map(beers => <BeerCard {...beers} />)}
     </div>
   )
 }
